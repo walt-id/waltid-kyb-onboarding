4ba19c7cde69d7664fd16fda105abad4586ad9e8
SuperBatata <khaledbattiche@gmail.com>
2025-02-18 12:34:30 +0100

    updated business services

--- src/main/kotlin/services/business/BusinessService.kt
+++ src/main/kotlin/services/business/BusinessService.kt
@@ -1,21 +1,43 @@
 package id.walt.services.business
 
+import com.mongodb.client.model.Filters
 import com.mongodb.client.model.Filters.eq
+import com.mongodb.client.model.Updates
 import id.walt.database.Database
 import id.walt.models.business.Business
 import id.walt.models.business.BusinessDataSource
+import id.walt.models.business.CompanyStatus
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.toList
+import org.bson.types.ObjectId
 
 class BusinessService : BusinessDataSource {
     suspend fun isBusinessExisting(accountId: String): Boolean =
-        Database.accounts.countDocuments(eq("_id", accountId)) >= 1
+        Database.business.countDocuments(eq("_id", accountId)) >= 1
+
+
     override suspend fun createBusiness(business: Business): Business {
-//        Database.accounts.insertOne(business)
-//        return business
-        TODO("Not yet implemented")
+        if (isBusinessExisting(business.registration_number)) {
+            error("Business with ${business.registration_number} already exists")
+        }
+        Database.business.insertOne(business)
+        return business
+    }
+
+    suspend fun getPendingBusiness(): List<Business> {
+      return  Database.business.find(eq("status", CompanyStatus.PENDING)).toList()
+    }
+
+    suspend fun updateCompanyStatus(businessId: ObjectId, newStatus: CompanyStatus): Boolean {
+        val result = Database.business.updateOne(
+            eq("_id", businessId),
+            Updates.set("status", newStatus.name)
+        )
+        return result.modifiedCount > 0
     }
 
     override suspend fun getBusinessById(id: String): Business? {
-        TODO("Not yet implemented")
+        return Database.business.find(eq("_id", id)).first()
     }
 
     override suspend fun deleteBusinessById(id: String): Boolean {
