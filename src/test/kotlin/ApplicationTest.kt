8172c57fc00afc55f0692f4cf8b44793a41cc5a9
SuperBatata <khaledbattiche@gmail.com>
2025-02-18 12:39:00 +0100

    removed default test

--- src/test/kotlin/ApplicationTest.kt
+++ src/test/kotlin/ApplicationTest.kt
@@ -2,20 +2,19 @@ package id.walt
 
 import io.ktor.client.request.*
 import io.ktor.http.*
-import io.ktor.server.testing.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class ApplicationTest {
 
-    @Test
-    fun testRoot() = testApplication {
-        application {
-            module()
-        }
-        client.get("/").apply {
-            assertEquals(HttpStatusCode.OK, status)
-        }
-    }
+//    @Test
+//    fun testRoot() = testApplication {
+//        application {
+//            module()
+//        }
+//        client.get("/").apply {
+//            assertEquals(HttpStatusCode.OK, status)
+//        }
+//    }
 
 }
