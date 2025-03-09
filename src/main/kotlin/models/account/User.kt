1bbe1ccffb59cffd07b5f80f7bdb6b7772d2a071
SuperBatata <khaledbattiche@gmail.com>
2025-03-09 21:11:58 +0100

    feat : added dataspace field for user

--- src/main/kotlin/models/account/User.kt
+++ src/main/kotlin/models/account/User.kt
@@ -1,19 +1,18 @@
 package id.walt.models.user
 
-import kotlinx.serialization.Contextual
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import org.bson.types.ObjectId
-import kotlin.uuid.ExperimentalUuidApi
 
 
 @Serializable
-data class Account @OptIn(ExperimentalUuidApi::class) constructor(
+data class Account(
     @SerialName("_id")
-    @Contextual val id: ObjectId = ObjectId(),
+    val id: String = ObjectId().toHexString(),
     val email: String,
     val password: String,
     val company: String,
     val role: String,
+    val dataSpace: String
 )
 
