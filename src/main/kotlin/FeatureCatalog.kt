25e4b6e1e1c77ff97c659af6536468f540009d4e
SuperBatata <khaledbattiche@gmail.com>
2025-04-24 23:47:57 +0100

    added multiple configs

--- src/main/kotlin/FeatureCatalog.kt
+++ src/main/kotlin/FeatureCatalog.kt
@@ -4,14 +4,16 @@ import id.walt.commons.featureflag.BaseFeature
 import id.walt.commons.featureflag.OptionalFeature
 import id.walt.commons.featureflag.ServiceFeatureCatalog
 import id.walt.database.DatabaseConfiguration
+import id.walt.services.credentials.IssuerConfiguration
 import id.walt.web.auth.AuthenticationConfiguration
 
 object FeatureCatalog : ServiceFeatureCatalog {
 
     val databaseFeature = BaseFeature("database", "Database", DatabaseConfiguration::class)
     val authenticationFeature = BaseFeature("auth", "Authentication", AuthenticationConfiguration::class)
+    val issuerFeature = BaseFeature("issuer", "Issuer", IssuerConfiguration::class)
 
-    override val baseFeatures = listOf(databaseFeature, authenticationFeature)
+    override val baseFeatures = listOf(databaseFeature, authenticationFeature, issuerFeature)
     override val optionalFeatures = listOf<OptionalFeature>()
 
 }
