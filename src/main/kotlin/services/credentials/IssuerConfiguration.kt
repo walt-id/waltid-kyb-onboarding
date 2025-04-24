d2beea915c30cf9bd8e9e30b2fa34b4eab2f5d4b
SuperBatata <khaledbattiche@gmail.com>
2025-04-24 23:48:56 +0100

    issuer config data class

---
+++ src/main/kotlin/services/credentials/IssuerConfiguration.kt
@@ -0,0 +1,16 @@
+package id.walt.services.credentials
+
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonObject
+
+@Serializable
+data class IssuerConfiguration(
+    val issuerKey: JsonObject,
+    val issuerUrl: String,
+    val issuerDid: String,
+)
+
+@Serializable
+data class WalletConfiguration(
+    val walletUrl: String,
+)
