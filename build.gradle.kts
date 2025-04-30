4c25de3e3fa9c662fccf78c534e26c5ba34ef965
SuperBatata <khaledbattiche@gmail.com>
2025-04-30 19:05:37 +0100

    fixed build.gradle.kts file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,11 @@
-
 plugins {
     kotlin("jvm") version "2.1.10"
-    id("io.ktor.plugin") version "2.3.13"
+    id("io.ktor.plugin") version "3.1.2"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.1.10"
     application
 }
 
 
-object Versions {
-
-    const val WALTID_VERSION = "0.12.0"
-
-    const val ktorVersion = "3.1.2"
-}
 group = "id.walt"
 version = "0.0.1"
 
@@ -32,46 +25,19 @@ repositories {
     maven("https://maven.waltid.dev/snapshots")
 }
 
+
+
+object Versions {
+    const val KTOR_VERSION = "3.1.0" // also change 1 plugin
+    const val WALTID_VERSION = "0.12.0"//"1.0.2503041241-SNAPSHOT"//"1.0.2503041428-SNAPSHOT"//""0.11.0"
+}
+
 dependencies {
-
-    // DB
-    //MongoDB
-    implementation("org.mongodb:mongodb-driver-kotlin-coroutine:5.2.0")
-    implementation("org.mongodb:bson-kotlinx:5.2.0")
-    // Ktor Server
-    implementation("io.ktor:ktor-client-core:${Versions.ktorVersion}")
-    implementation("io.ktor:ktor-client-cio:${Versions.ktorVersion}")
-    implementation("io.ktor:ktor-server-core-jvm:${Versions.ktorVersion}")
-    implementation("io.ktor:ktor-server-auth-jvm:${Versions.ktorVersion}")
-    implementation("io.ktor:ktor-server-host-common-jvm:${Versions.ktorVersion}")
-    implementation("io.ktor:ktor-server-status-pages-jvm:${Versions.ktorVersion}")
-    implementation("io.ktor:ktor-server-cors-jvm:${Versions.ktorVersion}")
-    implementation("io.ktor:ktor-server-default-headers-jvm:${Versions.ktorVersion}")
-    implementation("io.ktor:ktor-server-openapi:${Versions.ktorVersion}")
-    implementation("io.ktor:ktor-server-swagger:${Versions.ktorVersion}")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:${Versions.ktorVersion}")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:${Versions.ktorVersion}")
-    implementation("io.ktor:ktor-serialization-gson-jvm:${Versions.ktorVersion}")
-    implementation("io.ktor:ktor-server-call-logging-jvm:${Versions.ktorVersion}")
-    implementation("io.ktor:ktor-server-call-id-jvm:${Versions.ktorVersion}")
-    implementation("io.ktor:ktor-server-double-receive-jvm:${Versions.ktorVersion}")
-    implementation("io.ktor:ktor-server-cio-jvm:${Versions.ktorVersion}")
-    implementation("io.ktor:ktor-server-auto-head-response-jvm:${Versions.ktorVersion}")
-    implementation("io.ktor:ktor-server-auth-jwt-jvm:${Versions.ktorVersion}")
-    implementation("io.ktor:ktor-server-sessions-jvm:${Versions.ktorVersion}")
-
-    // Ktor Client
-    implementation("io.ktor:ktor-client-core-jvm:${Versions.ktorVersion}")
-    implementation("io.ktor:ktor-client-content-negotiation:${Versions.ktorVersion}")
-    implementation("io.ktor:ktor-client-serialization-jvm:${Versions.ktorVersion}")
-    implementation("io.ktor:ktor-client-json-jvm:${Versions.ktorVersion}")
-    implementation("io.ktor:ktor-client-cio-jvm:${Versions.ktorVersion}")
-    implementation("io.ktor:ktor-client-logging-jvm:${Versions.ktorVersion}")
-
-    // Ktor Swagger
+// Ktor Swagger
+    implementation("io.ktor:ktor-server-openapi:${Versions.KTOR_VERSION}")
+    implementation("io.ktor:ktor-server-swagger:${Versions.KTOR_VERSION}")
     implementation("io.github.smiley4:ktor-swagger-ui:2.3.1")
-
-    // Date/Time & Cryptography
+    api("io.github.smiley4:ktor-swagger-ui:3.5.1")
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
     implementation(platform("org.kotlincrypto.hash:bom:0.6.1"))
     implementation("org.kotlincrypto.hash:sha2")
@@ -80,7 +46,61 @@ dependencies {
     implementation("io.github.oshai:kotlin-logging:5.1.0")
     // Security
     implementation("org.mindrot:jbcrypt:0.4")
-    // WaltID dependencies
+
+
+
+    implementation("org.mongodb:mongodb-driver-kotlin-coroutine:5.2.0")
+    implementation("org.mongodb:bson-kotlinx:5.2.0")
+
+    // Ktor server
+    implementation("io.ktor:ktor-server-core-jvm:${Versions.KTOR_VERSION}")
+    implementation("io.ktor:ktor-server-auth-jvm:${Versions.KTOR_VERSION}")
+    implementation("io.ktor:ktor-server-double-receive-jvm:${Versions.KTOR_VERSION}")
+    implementation("io.ktor:ktor-server-auto-head-response-jvm:${Versions.KTOR_VERSION}")
+    implementation("io.ktor:ktor-server-host-common-jvm:${Versions.KTOR_VERSION}")
+    implementation("io.ktor:ktor-server-status-pages-jvm:${Versions.KTOR_VERSION}")
+    implementation("io.ktor:ktor-server-cors-jvm:${Versions.KTOR_VERSION}")
+    implementation("io.ktor:ktor-server-forwarded-header-jvm:${Versions.KTOR_VERSION}")
+    implementation("io.ktor:ktor-server-call-logging-jvm:${Versions.KTOR_VERSION}")
+    implementation("io.ktor:ktor-server-call-id-jvm:${Versions.KTOR_VERSION}")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:${Versions.KTOR_VERSION}")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:${Versions.KTOR_VERSION}")
+    implementation("io.ktor:ktor-server-cio-jvm:${Versions.KTOR_VERSION}")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:${Versions.KTOR_VERSION}")
+    implementation("io.ktor:ktor-server-status-pages-jvm:${Versions.KTOR_VERSION}")
+    implementation("io.ktor:ktor-server-compression-jvm:${Versions.KTOR_VERSION}")
+
+    // ktor-client
+    implementation("io.ktor:ktor-client-core:${Versions.KTOR_VERSION}") // Or latest version
+    implementation("io.ktor:ktor-client-cio:${Versions.KTOR_VERSION}") // Or another engine
+    implementation("io.ktor:ktor-client-content-negotiation:${Versions.KTOR_VERSION}")
+    implementation("io.ktor:ktor-client-logging:${Versions.KTOR_VERSION}")
+
+    // OpenAPI
+    api("io.github.smiley4:ktor-swagger-ui:3.5.1")
+
+    // JSON
+    implementation("io.github.optimumcode:json-schema-validator:0.2.3")
+    implementation("com.eygraber:jsonpathkt-kotlinx:3.0.2")
+    implementation("io.github.reidsync:kotlin-json-patch:1.0.0")
+
+    // Kotlinx
+    implementation("app.softwork:kotlinx-uuid-core:0.1.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.1")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+
+    // DB
+    implementation("org.mongodb:mongodb-driver-kotlin-coroutine:5.2.0")
+    implementation("org.mongodb:bson-kotlinx:5.2.0")
+
+    // Logging
+    implementation("io.klogging:klogging:0.9.4")
+    implementation("io.klogging:slf4j-klogging:0.9.4")
+    implementation("org.slf4j:jul-to-slf4j:2.0.16")
+
+    // walt.id
     implementation("id.walt.crypto:waltid-crypto:${Versions.WALTID_VERSION}")
     implementation("id.walt.credentials:waltid-verifiable-credentials:${Versions.WALTID_VERSION}")
     implementation("id.walt.did:waltid-did:${Versions.WALTID_VERSION}")
@@ -92,63 +112,14 @@ dependencies {
     implementation("id.walt.dif-definitions-parser:waltid-dif-definitions-parser:${Versions.WALTID_VERSION}")
     implementation("id.walt.wallet:waltid-core-wallet:${Versions.WALTID_VERSION}")
 
-    // Testing
+
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test-jvm:1.9.0")
     testImplementation(kotlin("test"))
-    api("io.github.smiley4:ktor-swagger-ui:3.5.1")
+    testImplementation("io.ktor:ktor-server-test-host:2.3.12")
+    testImplementation("io.ktor:ktor-client-cio:2.3.12")
+    testImplementation("io.ktor:ktor-client-content-negotiation:2.3.12")
+    testImplementation("io.ktor:ktor-client-logging:2.3.12")
+
 }
 
-//fun MavenArtifactRepository.configureCredentials() {
-//    val repoName = "MAVEN"
-//    val (username, password) = waltidPrivateCredentials(repoName)
-//    require(username.isNotEmpty()) { getMissingRepoCredentialsErrorMessage(repoName, "username") }
-//    require(password.isNotEmpty()) { getMissingRepoCredentialsErrorMessage(repoName, "password") }
-//    credentials {
-//        this.username = username
-//        this.password = password
-//    }
-//}
-//fun waltidPrivateCredentials(repoName:String): Pair<String, String> = let {
-//    val envUsername = System.getenv(repoName.uppercase() + "_USERNAME")
-//    val envPassword = System.getenv(repoName.uppercase() + "_PASSWORD")
-//
-//    val usernameFile = File("$rootDir/secret-${repoName.lowercase()}-username.txt")
-//    val passwordFile = File("$rootDir/secret-${repoName.lowercase()}-password.txt")
-//
-//    return Pair(
-//        envUsername ?: usernameFile.let { if (it.isFile) it.readLines().first() else "" },
-//        envPassword ?: passwordFile.let { if (it.isFile) it.readLines().first() else "" }
-//    )
-//}
-// fun getMissingRepoCredentialsErrorMessage(repo: String, field: String) =
-//    """
-//        Missing $repo credentials.
-//        Expecting non-empty value in:
-//            - environment variable: ${repo.uppercase()}_${field.uppercase()}
-//            - or text file in the project root: secret-${repo.lowercase()}-${field.lowercase()}.txt
-//    """.trimIndent()
-//ktor {
-//    fatJar {
-//        archiveFileName.set("kyb-onboarding.jar")
-//    }
 
-//    docker {
-//        jreVersion.set(JavaVersion.VERSION_21)
-//        localImageName.set("waltid/kyb-onboarding")
-//        imageTag.set("1.0.0-SNAPSHOT")
-//        portMappings.set(listOf(
-//            io.ktor.plugin.features.DockerPortMapping(
-//                3000,
-//                3000,
-//                io.ktor.plugin.features.DockerPortMappingProtocol.TCP
-//            )
-//        ))
-//
-//        externalRegistry.set(
-//            io.ktor.plugin.features.DockerImageRegistry.dockerHub(
-//                appName = provider { "kyb-onboarding" },
-//                username = provider { "waltid" },
-//                password = provider { "If87v%u%cVBB" }
-//            )
-//        )
-//    }
-//}
